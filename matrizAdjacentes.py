ab201336a5531cdd32ef00f4b6a79e2c1386e26a
ErickFiel <89852168+ErickFiel@users.noreply.github.com>
2023-05-09 23:00:54 -0300

    Update matrizAdjacentes.py

--- matrizAdjacentes.py
+++ matrizAdjacentes.py
@@ -1,14 +1,3 @@
-"""
-Iniciando nossa API, implemente representações computaciona de Grafos por meio de Listas de Adjacências e Matrizes de Adjacências.
-
-Seguindo nossa API e a implementação computacional realizada na tarefa anterior, implemente uma classe Grafos que deve conter os sequintes métodos: 
-a) V() retorna o número de vértices (feito)
-b) A() retorna o número de arestas (feito) 
-c) AddArestas() - Adiciona arestas (feito)
-d) Adj() retorna arestas adjacentes (arestas que possuem vertices em comum)
-
-"""
-
 class Grafos:
 
     def __init__(self, vertices):
@@ -16,10 +5,22 @@ class Grafos:
         self.vertices = vertices
         self.arestas = 0
         self.arestas_adjacentes = [[] for i in range (self.vertices)]
-        self.grafo = [[0] * self.vertices for i in range(self.vertices)] 
+        self.grafo = [[0] * self.vertices for i in range(self.vertices)]
+        
+        self.cor = [[] for i in range (self.vertices)]
+        self.antecessor = [[] for i in range (self.vertices)]
+        self.tempo = 0
+        self.tempo_inicial = [[] for i in range (self.vertices)]
+        self.tempo_final = [[] for i in range (self.vertices)]
+        self.lista_DFS = [[] for i in range (self.vertices)]   
     
     def V(self):
+        #Retorna o numero de vertices
         return self.vertices
+    
+    def A(self):
+        #Retorna o numero de arestas
+        return self.arestas
 
     def AddArestas(self, u, v):
         #Adicionar aresta no grafo
@@ -30,14 +31,32 @@ class Grafos:
         self.arestas += 1
     
     def Adj(self):
-        return self.arestas_adjacentes
+        #Retorna a arestas adjacentes
+        return self.arestas_adjacentes  
 
     def Printar_Grafo(self):
+        #Mostrar o grafo
         print("Grafo:")
         for i in range(self.vertices):
             print(self.grafo[i])
 
-grafo = Grafos(3)
-grafo.AddArestas(0, 2)
-grafo.Printar_Grafo()
-print(grafo.Adj())    
+    def DFS_Visita(self, u):
+        self.cor[u] = "CINZA"
+        self.lista_DFS[u].append(u)
+        self.tempo = self.tempo + 1
+        self.tempo_inicial[u] = self.tempo
+        for v in self.arestas_adjacentes[u]:
+            if self.cor[u][v] == "BRANCO":
+                self.antecessor[v] = u
+                self.DFS_Visita(v)
+        self.cor[u] = "PRETO"
+        self.tempo_final[u] = self.tempo + 1 
+
+
+    def DFS(self):
+        for i in range(self.vertices):
+            self.cor[i] = "BRANCO"
+        for w in range(self.vertices):
+            if self.cor[w] == "BRANCO":
+                self.DFS_Visita(w)
+
