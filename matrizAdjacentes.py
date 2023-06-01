0f466981c044d364cb1a17cb973f1246ec6c4325
ErickFiel <89852168+ErickFiel@users.noreply.github.com>
2023-05-31 21:04:33 -0300

    Update matrizAdjacentes.py

--- matrizAdjacentes.py
+++ matrizAdjacentes.py
@@ -1,10 +1,12 @@
 class Grafos:
 
     def __init__(self, vertices):
+        #Informações do grafo
         self.vertices = vertices
         self.arestas = 0
         self.arestas_adjacentes = [[] for i in range(self.vertices)]
         self.grafo = [[0] * self.vertices for i in range(self.vertices)]
+        
         #Informações do DFS
         self.tempo = 0
         self.cor = [[] for i in range (self.vertices)]
@@ -12,31 +14,52 @@ class Grafos:
         self.tempo_inicial = [[] for i in range (self.vertices)]
         self.tempo_final = [[] for i in range (self.vertices)]
         self.lista_DFS = [[] for i in range (self.vertices)]   
+
         #Informações do BFS
         self.antecessorBFS = [None for i in range (self.vertices)]
         self.corBFS = [[] for i in range (self.vertices)]
         self.distancia = [[] for i in range (self.vertices)]
-        self.filaQ = []       
+        self.filaQ = []
+        
+        #Relaxamento
+        self.antecessorDij = [None for i in range (self.vertices)]
+        self.d = [[] for i in range (self.vertices)]
+        self.s = [] 
+        self.filaDijkstra = [[] for i in range (self.vertices)] 
+        
+        #Floyd
+        self.matrizFloyd = [None for i in range (self.vertices)] 
+        self.antecessorFloyd = [None for i in range (self.vertices)]
     
+    #Retorna o número de vértices
     def V(self):
-        #Retorna o número de vertices
         return self.vertices
     
+    #Retorna o número de arestas
     def A(self):
-        #Retorna o numero de arestas
         return self.arestas
 
+    #Adicionar arestas no grafo
     def AddArestas(self, u, v):
-        #Adicionar aresta no grafo
-        self.grafo[u][v] = 1
-        self.grafo[v][u] = 1
-        #Ajustar quando os vertices forem iguais
-        self.arestas_adjacentes[u].append((u,v))
-        self.arestas_adjacentes[v].append((v,u))
-        self.arestas += 1
+        if u == v:
+            if (u, v) in self.arestas_adjacentes[u]:
+                print("Laço já existe.")
+                return
+            self.grafo[u][v] = 1
+            self.arestas_adjacentes[u].append((u, v))
+            self.arestas += 1
+        else:
+            if (u, v) in self.arestas_adjacentes[u] or (v, u) in self.arestas_adjacentes[u]:
+                print("Aresta já existe.")
+                return
+            self.grafo[u][v] = 1
+            self.grafo[v][u] = 1
+            self.arestas_adjacentes[u].append((u, v))
+            self.arestas_adjacentes[v].append((v, u))
+            self.arestas += 1
     
+    #Retorna a arestas adjacentes
     def Adj(self):
-        #Retorna a arestas adjacentes
         return self.arestas_adjacentes  
 
     def printGrafo(self):
@@ -90,6 +113,61 @@ class Grafos:
                     self.filaQ.append(w)
             self.corBFS[u] = "PRETO"
 
+    def SingleSourse(self, s):
+        for i in range(self.vertices):
+            self.d[i] = float('inf')
+            #self.antecessorDij[i] = None
+        self.d[s] = 0
+    
+    def Relax(self, u, v):
+        if self.d[v] > self.d[u] + self.grafo[u][v]:
+            self.d[v] = self.d[u] + self.grafo[u][v]
+            self.antecessorDij[v] = u
+    
+    def Dijkstra(self, s):
+        self.SingleSourse(s)
+        for i in range(self.vertices):
+            self.filaDijkstra[i] = i
+        while(self.filaDijkstra != []):
+            u = self.filaDijkstra[0]
+            del self.filaDijkstra[0]
+            self.s.append(u)
+            for i in range(self.vertices):
+                if self.grafo[u][i] != 0:
+                    self.Relax(u, i)  
+    
+    def BellmanFord(self, s):
+        self.SingleSourse(self, s)
+        for i in range(self.vertices - 1):
+            for u, v in self.grafo:
+                self.Relax(u, v)
+        for u, v in self.grafo:
+            if self.d[v] > self.d[u] + self.grafo[u][v]:
+                return True
+        return False
+
+    def FloydWarshall(self):
+        for v in range(self.vertices):
+            for w in range(self.vertices):
+                if v == w:
+                    self.matrizFloyd[v][w] = 0
+                self.matrizFloyd[v][w] = self.grafo[v][w]
+        for k in range(self.vertices):
+            for v in range(self.vertices):
+                for w in range(self.vertices):
+                    if (self.matrizFloyd[v][k] + self.matrizFloyd[k][w] < self.matrizFloyd[v][w]):
+                        self.matrizFloyd[v][w] = self.matrizFloyd[v][k] + self.matrizFloyd[k][w]
+                        self.antecessorFloyd[w] = k
+
+    def ComponentesConectados(self):
+        ListaComp = []
+        for v in range(self.vertices):
+            if not self.visitado[v]:
+                componente = []
+                self.DFS(v, componente)
+                ListaComp.append(componente)
+        return ListaComp               
+
     def printDFS(self):
         for i in range(self.vertices):
             print(f"| Ordem da lista -> {grafo.lista_DFS[i]} | Antecessor -> {grafo.antecessor[i]} | Tempo Inicial -> {grafo.tempo_inicial[i]} | Tempo Final -> {grafo.tempo_final[i]} |")
@@ -98,3 +176,7 @@ class Grafos:
     def printBFS(self):
         for i in range(self.vertices):
             print(f"Vertice --> {i} | Antecessor --> {self.antecessorBFS[i]} | Distancia --> {self.distancia[i]} |")
+    
+    def printDij(self):
+        for i in range(self.vertices):
+            print(self.s[i])
